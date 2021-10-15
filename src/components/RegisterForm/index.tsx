dfb44d8f7aca3eef4d178f93b7ec7cb2e7f042da
senaarth <senaarth@gmail.com>
2021-10-14 21:18:12 -0300

    fix: updating login page styles

--- src/components/RegisterForm/index.tsx
+++ src/components/RegisterForm/index.tsx
@@ -12,11 +12,11 @@ export function RegisterForm() {
         <label htmlFor="sobrenome">Sobrenome</label>
         <input className="inputReg" type="text" />
         <label htmlFor="email">E-mail</label>
-        <input className="inputReg" type="email" />
+        <input className="inputReg" type="email" placeholder="user@email.com" />
         <label htmlFor="senha">Senha</label>
-        <input className="inputReg" type="text" />
+        <input className="inputReg" type="password" placeholder="******" />
         <label htmlFor="senha">Confirme Sua Senha</label>
-        <input className="inputReg" type="text" />
+        <input className="inputReg" type="password" placeholder="******" />
         <button className="buttonReg">Cadastrar-se</button>
       </Form>
     </div>
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,20 +1,33 @@
 import React from "react";
 import { RegisterForm } from "../../components/RegisterForm/";
 import { Form } from "react-bootstrap";
+
 import "./style.css";
 
 export function Login() {
   return (
     <div className="loginBody">
-      <RegisterForm />
-      <Form className="col-6 form_login">
-        <h2>Login</h2>
-        <label htmlFor="nome">Nome</label>
-        <input className="inputLogin" type="text" />
-        <label htmlFor="senha">Senha</label>
-        <input className="inputLogin" type="text" />
-        <button className="buttonLogin">Entrar</button>
-      </Form>
+      <div className="reg-container">
+        <RegisterForm />
+      </div>
+      <div className="login-container">
+        <Form className="col-6 form_login">
+          <h2>Login</h2>
+          <label htmlFor="nome">Seu e-mail</label>
+          <input
+            className="inputLogin"
+            type="text"
+            placeholder="user@email.com"
+          />
+          <label htmlFor="senha">Senha</label>
+          <input
+            className="inputLogin"
+            type="password"
+            placeholder="********"
+          />
+          <button className="buttonLogin">Entrar</button>
+        </Form>
+      </div>
     </div>
   );
 }
